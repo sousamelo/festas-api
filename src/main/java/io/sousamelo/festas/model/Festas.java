7790bbd521a3a3f4cd23a78f0015a51d07e4fcdc
sousamelo <rogeriomelo@outlook.com>
2021-03-31 17:03:37 -0300

    Criando modelo para Festa

---
+++ src/main/java/io/sousamelo/festas/model/Festas.java
@@ -0,0 +1,36 @@
+package io.sousamelo.festas.model;
+
+public class Festas {
+
+    public String nome;
+    public String local;
+    public String data;
+    public String horario;
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getLocal() {
+        return local;
+    }
+    public void setLocal(String local) {
+        this.local = local;
+    }
+    public String getData() {
+        return data;
+    }
+    public void setData(String data) {
+        this.data = data;
+    }
+    public String getHorario() {
+        return horario;
+    }
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    
+}
