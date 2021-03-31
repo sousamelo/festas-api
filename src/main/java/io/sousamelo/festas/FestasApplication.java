5a9e6ffa27c89a1f95eee5c4b851ec373c420006
sousamelo <rogeriomelo@outlook.com>
2021-03-31 16:44:36 -0300

    Arquivos iniciais da aplicação

---
+++ src/main/java/io/sousamelo/festas/FestasApplication.java
@@ -0,0 +1,13 @@
+package io.sousamelo.festas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/sousamelo/festas/FestasApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sousamelo.festas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
